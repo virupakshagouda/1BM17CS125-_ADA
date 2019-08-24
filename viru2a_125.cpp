8f606806d1ccdcfce676e53d69c7a2271bc8692c
virupakshagouda <52944329+virupakshagouda@users.noreply.github.com>
2019-08-24 10:42:12 +0530

    Add files via upload

---
+++ viru2a_125.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[10],n;
+	cout<<"enter the no' of elements u want to enter";
+	cin>>n;
+	cout<<"enter array elements";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n; i++)
+	{
+		for(int j=i+1; j<n; j++)
+		{
+			if(a[i]>a[j])
+			{
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+		
+	}
+	/*
+	cout<<"enter l"<<endl;;
+	int l;
+	cin>>l;
+	for(int i=n-l; i<n; i++)
+	{
+		cout<<a[i]<<endl;
+	}
+	
+	
+	cout<<"your sorted array elements are"<<endl;
+	for(int i=0;i<n; i++)
+	{
+		cout<<a[i]<<endl;
+	}
+	 * */
+	cout<<"enter k";
+	int k;
+	cin>>k;
+	cout<<a[k-1];
+	
+	
+	return 0;
+}
